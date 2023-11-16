6abd034901c2cc6553d83b059dfacea5a52de9bb
ilyapatis24 <ilyapatis24@mail.ru>
2023-11-17 00:06:33 +0300

    Structures_mission1

---
+++ 02/main.cpp
@@ -0,0 +1,35 @@
+﻿# include <iostream>
+#include <locale.h>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+struct Person {
+	string name = "";
+	int account_number = 0;
+	double balance = 0;
+};
+
+void person_info(Person& person) {
+	cout << "Введите номер счёта: ";
+	cin >> person.account_number;
+	cout << endl;
+	cout << "Введите имя владельца: ";
+	cin >> person.name;
+	cout << endl;
+	cout << "Введите баланс: ";
+	cin>> person.balance;
+	cout << endl;
+	cout << "Введите новый баланс: ";
+	cin >> person.balance;
+	cout << endl;
+	cout << "Ваш счет: " << person.name << ", " << person.account_number << ", " << person.balance;
+}
+
+int main(int argc, char** argv) {
+	setlocale(LC_ALL, "Russian");
+	struct Person person;
+	person_info(person);
+	return 0;
+}
