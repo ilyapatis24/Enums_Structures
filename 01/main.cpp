1789cf811300e0cf73c5c09bd9255be95bbb923e
ilyapatis24 <ilyapatis24@mail.ru>
2023-11-16 21:06:13 +0300

    Initial commit

---
+++ 01/main.cpp
@@ -0,0 +1,57 @@
+# include <iostream>
+#include <locale.h>
+
+
+enum Months {
+	january = 1,
+	february,
+	march,
+	april,
+	may,
+	june,
+	july,
+	august,
+	september,
+	october,
+	november,
+	december
+};
+
+
+void get_month() {
+	int user_month = 0;
+	Months month;
+	do {
+		std::cout << "Введите номер месяца:" << " " << std::endl;
+		std::cin >> user_month;
+		month = static_cast<Months>(user_month);
+		switch (month) {
+			case january: std::cout << "Январь" << std::endl; break;
+			case february: std::cout << "Февраль" << std::endl; break;
+			case march: std::cout << "Март" << std::endl; break;
+			case april: std::cout << "Апрель" << std::endl; break;
+			case may: std::cout << "Май" << std::endl; break;
+			case june: std::cout << "Июнь" << std::endl; break;
+			case july: std::cout << "Июль" << std::endl; break;
+			case august: std::cout << "Август" << std::endl; break;
+			case september: std::cout << "Сентябрь" << std::endl; break;
+			case october: std::cout << "Октябрь" << std::endl; break;
+			case november: std::cout << "Ноябрь" << std::endl; break;
+			case december: std::cout << "Декабрь" << std::endl; break;
+			default:
+				if (month == 0) {
+					break;
+				}
+				else { std::cout << "Неправильный номер!" << std::endl; }
+
+		}
+	} 
+	while (user_month != 0);
+	std::cout << "До свидания";
+}
+
+int main(int argc, char** argv) {
+	setlocale(LC_ALL, "Russian");
+	get_month();
+	return 0;
+}
